d8078386c8fd6d1c0d54a2685e857c079656c730
bmarinov3301 <bozhidar.marinov.3301@gmail.com>
2024-09-18 12:21:25 +0300

    add constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,7 @@
 export const resourcePrefix = '';
 export const lambdaCustomHeaderName = '';
 export const lambdaCustomHeaderValue = '';
-export const restApiAllowedOrigins = [];
+export const restApiAllowedOrigins = [''];
+export const pdfTemplateFile = '';
+export const pdfDataEndpoint = '';
+export const userTimeZone = '';
